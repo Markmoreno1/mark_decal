be95a824925503f0b33f9088323eacc86abba805
mark <markmoreno@berkeley.edu>
2025-02-12 14:58:30 -0800

    Adding homework2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,24 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# cd Desktop or cd ~
 # 2) What does ~/ mean?
+# it means the home directory 
 # 3) What's the difference between and abolute and a relative path in your terminal?
+# an absolute path goes all the way from your home directory while a relitive path just goes from the directory youre currently in
 # 4) What command would bring you back to your home directory?
+# you would just use cd ~
 # 5) If you called rm ./ in your current working directory, what would it do? 
+# i belive it can destroy everything in your current working directory
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# git add moves a file from your working directory to the staging area git commit moves it to the local repository and git push copies it to a remote repository
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
-# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# when i put that in the terminal no it says On branch main
+#Your branch is up to date with 'origin/main'.
+
+#nothing to commit, working tree clean 
+#normally it would tell me the file is staged but has not been comitted
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it? 
+# the most annoying error so far are syntax errors since i have difficulty finding what caused the error to troubleshoot it i try to correct the line and re run the code
 # 9) How does cloning a repository differ from pulling from a repository?
+# cloning would be making a copy of the original repository while pulling would affect the original file
 # 10) Tell me a fun fact!
+# racoons try to clean their food and have been seen trying to wash food that disoleves in water like cotton candy making them sad
