542144f6960b79ff85206029cb49f6b4c153e2ed
mark <markmoreno@berkeley.edu>
2025-04-03 18:30:11 -0700

    adding math_tools.py

---
+++ math_tools.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a+b
+# adds a and b together
+def subtract(a, b):
+    return a-b
+# subtracts b from a
+def multiply(a, b):
+    return a*b
+# multiplies a and b
+def divide(a, b):
+    return a/b
+#divides a by b
